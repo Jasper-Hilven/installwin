f0ea9ff5c9057e1016c6126c05e42701b37d78d1
Jasper Hilven <jasperhilven@gmail.com>
2024-08-13 15:24:08 +0200

    Winget version fix

--- installWinget.ps1
+++ installWinget.ps1
@@ -1,17 +1,4 @@
-# Define the URL for the latest App Installer .appxbundle file
-$url = "https://github.com/microsoft/winget-cli/releases/latest/download/AppInstaller.appxbundle"
-
-# Define the path to save the downloaded file
-$outputPath = "$env:TEMP\AppInstaller.appxbundle"
-
-# Download the latest App Installer .appxbundle
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-# Install the App Installer .appxbundle
-Add-AppxPackage -Path $outputPath
-
-# Clean up
-Remove-Item -Path $outputPath
-
-# Verify installation and display the installed version of winget
-winget --version
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile winget.msixbundle
+Add-AppxPackage winget.msixbundle
+del winget.msixbundle
+winget source update
