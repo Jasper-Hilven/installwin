38e399082c02e0cd024b07b4ea4b168859d35335
Jasper Hilven <jasperhilven@gmail.com>
2024-08-13 14:55:05 +0200

    Latest version winget

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
     Write-Host "Installing"
 
-    & "$PSScriptRoot\installChoco.ps1"
-    & "$PSScriptRoot\installChocoApps.ps1"
     & "$PSScriptRoot\installWinget.ps1"
     & "$PSScriptRoot\installWingetApps.ps1"
+    & "$PSScriptRoot\installChoco.ps1"
+    & "$PSScriptRoot\installChocoApps.ps1"
--- installWinget.ps1
+++ installWinget.ps1
@@ -1,36 +1,17 @@
-# Check if winget is installed
-if (!(Get-Command "winget" -ErrorAction SilentlyContinue)) {
-    Write-Host "winget is not installed. Attempting to install it."
+# Define the URL for the latest App Installer .appxbundle file
+$url = "https://github.com/microsoft/winget-cli/releases/latest/download/AppInstaller.appxbundle"
 
-    # Define the URL for the App Installer package from Microsoft
-    $appInstallerUrl = "https://aka.ms/getwinget"
+# Define the path to save the downloaded file
+$outputPath = "$env:TEMP\AppInstaller.appxbundle"
 
-    # Download the App Installer
-    $downloadPath = "$env:TEMP\AppInstaller.zip"
-    Invoke-WebRequest -Uri $appInstallerUrl -OutFile $downloadPath
+# Download the latest App Installer .appxbundle
+Invoke-WebRequest -Uri $url -OutFile $outputPath
 
-    # Extract the zip file to a temporary directory
-    $extractPath = "$env:TEMP\AppInstaller"
-    Expand-Archive -Path $downloadPath -DestinationPath $extractPath -Force
+# Install the App Installer .appxbundle
+Add-AppxPackage -Path $outputPath
 
-    # Install the App Installer package
-    $appInstallerFile = Get-ChildItem -Path $extractPath -Filter "*.msixbundle" -Recurse | Select-Object -First 1
-    if ($appInstallerFile) {
-        Add-AppxPackage -Path $appInstallerFile.FullName
-        Write-Host "winget (App Installer) has been installed successfully."
-    } else {
-        Write-Host "Failed to find the App Installer package."
-    }
+# Clean up
+Remove-Item -Path $outputPath
 
-    # Clean up temporary files
-    Remove-Item -Path $downloadPath -Force
-    Remove-Item -Path $extractPath -Recurse -Force
-} else {
-    Write-Host "winget is already installed."
-}
-
-winget source remove msstore
-winget source add -n msstore -a https://storeedgefd.dsx.mp.microsoft.com/v9.0
-winget source remove winget
-winget source add -n winget -a https://cdn.winget.microsoft.com/cache
-winget source list
+# Verify installation and display the installed version of winget
+winget --version
